a0db685124cfc489b997f97826ceab772299309c
Seyi23nova <babatundeoluwaseyi777@gmail.com>
2025-02-07 20:56:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ import refreshTokenRoutes from './routes/refreshToken.js'
 import userRoutes from './routes/users.js'
 import { Server } from "socket.io";
 
-
-
 const app = express()
 
 const upload = multer({ dest: "uploads/" });
@@ -29,28 +27,8 @@ const PORT = process.env.PORT || 3000
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
      .then(() => {
           const server = app.listen(PORT, () => {
-              console.log(`The server is running on port ${PORT}`)
+              console.log(`server running on port ${PORT}`)
           })
-
-          const io = new Server(server)
-
-          let socketsConnected = new Set()
-
-          io.on('connection', onConnected)
-
-          function onConnected(socket){
-               console.log(socket.id)
-               socketsConnected.add(socket.id)
-
-               io.emit('clients-total', socketsConnected.size)
-
-               socket.on('disconnect', () => {
-                    console.log("socket disconnected", socket.id)
-                    socketsConnected.delete(socket.id)
-                    io.emit('clients-total', socketsConnected.size)
-               })
-          }
-
 
           return server
      }
